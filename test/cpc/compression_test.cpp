0f8caae126ee6c5cc37a30612128d110cece4111
saydakov <saydakov@yahoo-inc.com>
2018-08-27 10:36:35 -0700

    cleanup

--- test/cpc/compression_test.cpp
+++ test/cpc/compression_test.cpp
@@ -21,8 +21,7 @@ namespace datasketches {
 class compression_test: public CppUnit::TestFixture {
 
   CPPUNIT_TEST_SUITE(compression_test);
-  //CPPUNIT_TEST(compress_and_uncompress_pairs);
-  CPPUNIT_TEST(test);
+  CPPUNIT_TEST(compress_and_uncompress_pairs);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -55,8 +54,7 @@ public:
       }
     }
     int numPairs = nxt;
-    printf ("numPairs = %d\n", numPairs);
-
+    //printf ("numPairs = %d\n", numPairs);
     //  for (int i = 0; i < numPairs; i++) {
     //    printf ("%d: %d %d\n", i, pairArray[i] >> 6, pairArray[i] & 63);
     //  }
@@ -65,55 +63,10 @@ public:
 
     for (Long numBaseBits = 0; numBaseBits <= 11; numBaseBits++) {
       Long numWordsWritten = lowLevelCompressPairs(pairArray, (Long) numPairs, numBaseBits, compressedWords);
-      printf ("numWordsWritten = %lld (numBaseBits = %lld)\n", numWordsWritten, numBaseBits);
+      //printf ("numWordsWritten = %lld (numBaseBits = %lld)\n", numWordsWritten, numBaseBits);
 
       lowLevelUncompressPairs(pairArray2, (Long) numPairs, numBaseBits, compressedWords, numWordsWritten);
       for (int i = 0; i < numPairs; i++) {
-        assert (pairArray[i] == pairArray2[i]);
-      }
-    }
-  }
-
-  void test() {
-    const int N = 200;
-    const int MAXWORDS = 1000;
-
-    U64 twoHashes[2];
-    U32 pairArray[N];
-    U32 pairArray2[N];
-    int i;
-    for (i = 0; i < N; i++) {
-      getTwoRandomHashes(twoHashes);
-      U32 rand = twoHashes[0] & 0xffdf;
-      pairArray[i] = rand;
-    }
-    u32KnuthShellSort3(pairArray, 0L, (Long) (N-1));  // unsigned numerical sort
-    U32 prev = ALL32BITS;
-    int nxt = 0;
-    for (i = 0; i < N; i++) {     // uniquify
-      if (pairArray[i] != prev) {
-        prev = pairArray[i];
-        pairArray[nxt++] = pairArray[i];
-      }
-    }
-    int numPairs = nxt;
-    printf ("numPairs = %d\n", numPairs);
-
-    //  for (i = 0; i < numPairs; i++) {
-    //    printf ("%d: %d %d\n", i, pairArray[i] >> 6, pairArray[i] & 63);
-    //  }
-
-    U32 compressedWords [MAXWORDS];
-    Long bb; // numBaseBits
-
-    for (bb = 0; bb <= 11; bb++) {
-      Long numWordsWritten =
-        lowLevelCompressPairs (pairArray, (Long) numPairs, bb, compressedWords);
-      printf ("numWordsWritten = %lld (bb = %lld)\n", numWordsWritten, bb);
-
-      lowLevelUncompressPairs (pairArray2, (Long) numPairs, bb, compressedWords,
-             numWordsWritten);
-      for (i = 0; i < numPairs; i++) {
         assert (pairArray[i] == pairArray2[i]);
       }
     }
