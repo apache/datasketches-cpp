2ee5e1647607c2035cb613ea965ccc3c17acee0f
Jon Malkin <jmalkin@users.noreply.github.com>
2018-11-15 11:09:26 -0800

    Clean up config.mk

--- config.mk
+++ config.mk
@@ -2,18 +2,14 @@
 
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Darwin)
-  CC := clang++ -arch x86_64 -g3
-  #CC := g++-8 -g3
+  CC := clang++ -arch x86_64
   LIB_SUFFIX := dylib
 else
   CC := g++
   LIB_SUFFIX := so
 endif
 
-# -fwhole-program -flto
-# -Wextra
-COMMON_FLAGS := -O3 -fpic -DNDEBUG -flto 
-#COMMON_FLAGS := -O0 -g3 -Wall -pedantic -fpic
+COMMON_FLAGS := -O3 -fpic -Wall -pedantic -flto -fpic
 
 ifeq ($(UNAME_S),Linux)
     #CFLAGS += -std=gnu++11 -O2 # -fPIC
@@ -25,5 +21,4 @@ ifeq ($(UNAME_S),Linux)
 else
   CFLAGS += -x c $(COMMON_FLAGS)
   CPPFLAGS += -std=c++17 -stdlib=libc++ $(COMMON_FLAGS) -fwhole-program-vtables
-  #CPPFLAGS += -std=c++17 $(COMMON_FLAGS) -fwhole-program
 endif
