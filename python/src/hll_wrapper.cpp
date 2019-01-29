38cb1f8ef24f083f089d675b583e4c0fd760c561
Jon Malkin <jon.malkin@gmail.com>
2019-01-29 12:57:02 -0800

    more cmake changes, update python includes to assume correct include paths provided

--- python/src/hll_wrapper.cpp
+++ python/src/hll_wrapper.cpp
@@ -3,7 +3,7 @@
  * Apache License 2.0. See LICENSE file at the project root for terms.
  */
 
-#include "hll/include/hll.hpp"
+#include "hll.hpp"
 #include <boost/python.hpp>
 
 namespace bpy = boost::python;
