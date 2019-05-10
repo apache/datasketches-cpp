6426472c6d5431eb77e30eca5d02bee31e339d19
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2019-05-10 16:04:27 -0700

    removed unstructured test, added test for non-empty sketch with no retained keys in sampling mode

--- theta/test/theta_sketch_test.cpp
+++ theta/test/theta_sketch_test.cpp
@@ -13,8 +13,8 @@ namespace datasketches {
 class theta_sketch_test: public CppUnit::TestFixture {
 
   CPPUNIT_TEST_SUITE(theta_sketch_test);
-  CPPUNIT_TEST(test);
   CPPUNIT_TEST(empty);
+  CPPUNIT_TEST(non_empty_no_retained_keys);
   CPPUNIT_TEST(single_item);
   CPPUNIT_TEST(resize_exact);
   CPPUNIT_TEST(estimation);
@@ -31,61 +31,20 @@ class theta_sketch_test: public CppUnit::TestFixture {
   CPPUNIT_TEST(serialize_deserialize_stream_and_bytes_equivalency);
   CPPUNIT_TEST_SUITE_END();
 
-  void test() {
-    update_theta_sketch update_sketch = update_theta_sketch::builder().build();
-    std::cerr << "after build" << std::endl;
-    std::ofstream ofs("theta_update_empty.bin");
-    update_sketch.serialize(ofs);
-
-    CPPUNIT_ASSERT(update_sketch.is_empty());
-
-    int value = 1;
-    update_sketch.update(value++);
-    CPPUNIT_ASSERT(!update_sketch.is_empty());
-    CPPUNIT_ASSERT_EQUAL(1.0, update_sketch.get_estimate());
-
-    theta_sketch& sketch1 = update_sketch;
-    CPPUNIT_ASSERT_EQUAL(1.0, sketch1.get_estimate());
-
-    compact_theta_sketch compact_sketch = update_sketch.compact();
-    CPPUNIT_ASSERT_EQUAL(1.0, compact_sketch.get_estimate());
-    compact_sketch.to_stream(std::cerr, true);
-
-    theta_sketch& sketch2 = compact_sketch;
-    CPPUNIT_ASSERT_EQUAL(1.0, sketch2.get_estimate());
-
-    std::cerr << "copy construct" << std::endl;
-
-    update_theta_sketch sketch3 = update_sketch;
-
-    std::cerr << "move construct" << std::endl;
-
-    update_theta_sketch sketch4 = std::move(update_sketch);
-
-    std::cerr << "copy assign" << std::endl;
-
-    sketch3 = sketch4;
-
-    std::cerr << "move" << std::endl;
-
-    sketch3 = std::move(sketch4);
-
-    std::cerr << "end" << std::endl;
-
-    sketch3.update(value++);
-    sketch3.to_stream(std::cerr, true);
-
-    update_theta_sketch usk = update_theta_sketch::builder().build();
-    //for (int i = 0; i < 10000; i++) usk.update(i);
-    std::ofstream ofs2("theta_compact_empty.bin");
-    usk.compact().serialize(ofs2);
-  }
-
   void empty() {
     update_theta_sketch update_sketch = update_theta_sketch::builder().build();
     CPPUNIT_ASSERT(update_sketch.is_empty());
     CPPUNIT_ASSERT(!update_sketch.is_estimation_mode());
     CPPUNIT_ASSERT_EQUAL(1.0, update_sketch.get_theta());
+    CPPUNIT_ASSERT_EQUAL(0.0, update_sketch.get_estimate());
+  }
+
+  void non_empty_no_retained_keys() {
+    update_theta_sketch update_sketch = update_theta_sketch::builder().set_p(0.001).build();
+    update_sketch.update(1);
+    CPPUNIT_ASSERT_EQUAL(0U, update_sketch.get_num_retained());
+    CPPUNIT_ASSERT(!update_sketch.is_empty());
+    CPPUNIT_ASSERT(update_sketch.is_estimation_mode());
     CPPUNIT_ASSERT_EQUAL(0.0, update_sketch.get_estimate());
   }
 
