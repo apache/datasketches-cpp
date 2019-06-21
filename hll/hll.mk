056b60a26b8b58192ba400a6dc28d3811780d368
jmalkin <jmalkin@users.noreply.github.com>
2019-06-21 11:46:39 -0700

    Update cmake files to include theta, and clean up old hll makefile to build properly (including removing unused cpp files from hll)

--- hll/hll.mk
+++ hll/hll.mk
@@ -1,4 +1,3 @@
-HLL_SRCDIR := hll/src
 HLL_INCDIR := hll/include
 HLL_BUILDDIR := build/hll
 
@@ -8,23 +7,10 @@ HLL_TSTBUILD := hll/build
 HLL_TEST_BIN := hll_test
 HLL_TARGET := hll/$(HLL_TEST_BIN)
 
-HLL_CSOURCES := $(shell find $(HLL_SRCDIR) -type f -name "*.c")
-HLL_CPPSOURCES := $(shell find $(HLL_SRCDIR) -type f -name "*.cpp")
-HLL_SOURCES := $(HLL_CSOURCES) $(HLL_CPPSOURCES)
-HLL_OBJECTS := $(patsubst $(HLL_SRCDIR)/%,$(HLL_BUILDDIR)/%,$(HLL_CPPSOURCES:.cpp=.o))
-
 HLL_TSTSOURCES := $(shell find $(HLL_TSTDIR) -type f -name "*.cpp")
 HLL_TSTOBJS := $(patsubst $(HLL_TSTDIR)/%,$(HLL_TSTBUILD)/%,$(HLL_TSTSOURCES:.cpp=.o))
 
 HLL_INCLIST := $(COM_INCLIST) -I $(HLL_INCDIR)
-HLL_BUILDLIST := $(patsubst src/%,$(HLL_BUILDDIR)/%,$(HLL_INCDIR))
-
-hll: $(HLL_OBJECTS)
-
-$(HLL_BUILDDIR)/%.o: $(HLL_SRCDIR)/%.cpp
-	@mkdir -p $(HLL_BUILDDIR)
-	@echo "Compiling $<...";
-	@$(CC) $(CPPFLAGS) $(INC) $(HLL_INCLIST) -c -o $@ $<
 
 $(HLL_TSTBUILD)/%.o: $(HLL_TSTDIR)/%.cpp
 	@mkdir -p $(HLL_TSTBUILD)
@@ -36,9 +22,10 @@ hll_exec: $(COM_TSTOBJS) $(HLL_TSTOBJS)
 	@echo "Linking $(HLL_TARGET)..."
 	@$(CC) $^ -o $(HLL_TARGET) $(TSTLNKFLAGS) $(LIB)
 
-hll_test: $(LIBRARY) hll_exec
+hll_test: hll_exec
 	@cd hll; DYLD_LIBRARY_PATH=../$(TARGETDIR) ./$(HLL_TEST_BIN)
 
+.PHONY: hll_clean
 hll_clean:
 	@echo "Cleaning hll...";
 	@$(RM) -r $(HLL_BUILDDIR) $(HLL_TSTBUILD) $(HLL_TARGET)
