dcc45f287516a46f96d8b4d8df1a9a2d99812569
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2020-04-09 16:54:36 -0700

    docs

--- fi/include/frequent_items_sketch.hpp
+++ fi/include/frequent_items_sketch.hpp
@@ -47,26 +47,175 @@ template<typename A> using vector_u8 = std::vector<uint8_t, AllocU8<A>>;
 template<typename T, typename W = uint64_t, typename H = std::hash<T>, typename E = std::equal_to<T>, typename S = serde<T>, typename A = std::allocator<T>>
 class frequent_items_sketch {
 public:
-  explicit frequent_items_sketch(uint8_t lg_max_map_size);
-  frequent_items_sketch(uint8_t lg_start_map_size, uint8_t lg_max_map_size);
-  class row;
+
+  /**
+   * Construct this sketch with parameters lg_max_map_size and lg_start_map_size.
+   *
+   * @param lg_max_map_size Log2 of the physical size of the internal hash map managed by this
+   * sketch. The maximum capacity of this internal hash map is 0.75 times 2^lg_max_map_size.
+   * Both the ultimate accuracy and size of this sketch are functions of lg_max_map_size.
+   *
+   * @param lg_start_map_size Log2 of the starting physical size of the internal hash
+   * map managed by this sketch.
+   */
+  explicit frequent_items_sketch(uint8_t lg_max_map_size, uint8_t lg_start_map_size = LG_MIN_MAP_SIZE);
+
+  /**
+   * Update this sketch with an item and a positive weight (frequency count).
+   * @param item for which the weight should be increased (lvalue)
+   * @param weight the amount by which the weight of the item should be increased
+   * A count of zero is a no-op, and a negative count will throw an exception.
+   */
   void update(const T& item, W weight = 1);
+
+  /**
+   * Update this sketch with an item and a positive weight (frequency count).
+   * @param item for which the weight should be increased (rvalue)
+   * @param weight the amount by which the weight of the item should be increased
+   * A count of zero is a no-op, and a negative count will throw an exception.
+   */
   void update(T&& item, W weight = 1);
+
+  /**
+   * This function merges the other sketch into this one.
+   * The other sketch may be of a different size.
+   * @param other sketch to be merged into this (lvalue)
+   */
   void merge(const frequent_items_sketch& other);
+
+  /**
+   * This function merges the other sketch into this one.
+   * The other sketch may be of a different size.
+   * @param other sketch to be merged into this (rvalue)
+   */
   void merge(frequent_items_sketch&& other);
+
+  /**
+   * @return true if this sketch is empty
+   */
   bool is_empty() const;
+
+  /**
+   * @return the number of active items in the sketch
+   */
   uint32_t get_num_active_items() const;
+
+  /**
+   * Returns the sum of the weights (frequencies) in the stream seen so far by the sketch
+   *
+   * @return the total weight of all items in the stream seen so far by the sketch
+   */
   W get_total_weight() const;
+
+  /**
+   * Returns the estimate of the weight (frequency) of the given item.
+   * Note: The true frequency of a item would be the sum of the counts as a result of the
+   * two update functions.
+   *
+   * @param item the given item
+   * @return the estimate of the weight (frequency) of the given item
+   */
   W get_estimate(const T& item) const;
+
+  /**
+   * Returns the guaranteed lower bound weight (frequency) of the given item.
+   *
+   * @param item the given item.
+   * @return the guaranteed lower bound weight of the given item. That is, a number which
+   * is guaranteed to be no larger than the real weight.
+   */
   W get_lower_bound(const T& item) const;
+
+  /**
+   * Returns the guaranteed upper bound weight (frequency) of the given item.
+   *
+   * @param item the given item
+   * @return the guaranteed upper bound weight of the given item. That is, a number which
+   * is guaranteed to be no smaller than the real frequency.
+   */
   W get_upper_bound(const T& item) const;
+
+  /**
+   * @return An upper bound on the maximum error of get_estimate(item) for any item.
+   * This is equivalent to the maximum distance between the upper bound and the lower bound
+   * for any item.
+   */
   W get_maximum_error() const;
+
+  /**
+   * Returns epsilon value of this sketch.
+   * This is just the value <i>3.5 / max_map_size</i>.
+   * @return epsilon used by the sketch to compute error.
+   */
   double get_epsilon() const;
+
+  /**
+   * Returns epsilon used to compute <i>a priori</i> error.
+   * This is just the value <i>3.5 / maxMapSize</i>.
+   * @param maxMapSize the planned map size to be used when constructing this sketch.
+   * @return epsilon used to compute <i>a priori</i> error.
+   */
   static double get_epsilon(uint8_t lg_max_map_size);
+
+  /**
+   * Returns the estimated <i>a priori</i> error given the max_map_size for the sketch and the
+   * estimated_total_stream_weight.
+   * @param lg_max_map_size the planned map size to be used when constructing this sketch.
+   * @param estimated_total_stream_weight the estimated total stream weight.
+   * @return the estimated <i>a priori</i> error.
+   */
   static double get_apriori_error(uint8_t lg_max_map_size, W estimated_total_weight);
+
+  class row;
   typedef typename std::vector<row, typename std::allocator_traits<A>::template rebind_alloc<row>> vector_row; // alias for users
+
+  /**
+   * Returns an array of rows that include frequent items, estimates, upper and lower bounds
+   * given an error_type and using get_maximum_error() as a threshold.
+   *
+   * <p>The method first examines all active items in the sketch (items that have a counter).
+   *
+   * <p>If <i>error_type = NO_FALSE_NEGATIVES</i>, this will include an item in the result
+   * list if get_upper_bound(item) &gt; threshold.
+   * There will be no false negatives, i.e., no Type II error.
+   * There may be items in the set with true frequencies less than the threshold
+   * (false positives).</p>
+   *
+   * <p>If <i>error_type = NO_FALSE_POSITIVES</i>, this will include an item in the result
+   * list if get_lower_bound(item) &gt; threshold.
+   * There will be no false positives, i.e., no Type I error.
+   * There may be items omitted from the set with true frequencies greater than the
+   * threshold (false negatives).</p>
+   *
+   * @param error_type determines whether no false positives or no false negatives are desired.
+   * @return an array of frequent items
+   */
   vector_row get_frequent_items(frequent_items_error_type err_type) const;
+
+  /**
+   * Returns an array of rows that include frequent items, estimates, upper and lower bounds
+   * given an error_type and a threshold.
+   *
+   * <p>The method first examines all active items in the sketch (items that have a counter).
+   *
+   * <p>If <i>error_type = NO_FALSE_NEGATIVES</i>, this will include an item in the result
+   * list if get_upper_bound(item) &gt; threshold.
+   * There will be no false negatives, i.e., no Type II error.
+   * There may be items in the set with true frequencies less than the threshold
+   * (false positives).</p>
+   *
+   * <p>If <i>error_type = NO_FALSE_POSITIVES</i>, this will include an item in the result
+   * list if get_lower_bound(item) &gt; threshold.
+   * There will be no false positives, i.e., no Type I error.
+   * There may be items omitted from the set with true frequencies greater than the
+   * threshold (false negatives).</p>
+   *
+   * @param error_type determines whether no false positives or no false negatives are desired.
+   * @param threshold to include items in the result list
+   * @return an array of frequent items
+   */
   vector_row get_frequent_items(frequent_items_error_type err_type, W threshold) const;
+
   size_t get_serialized_size_bytes() const;
   void serialize(std::ostream& os) const;
   typedef vector_u8<A> vector_bytes; // alias for users
@@ -74,6 +223,7 @@ public:
   static frequent_items_sketch deserialize(std::istream& is);
   static frequent_items_sketch deserialize(const void* bytes, size_t size);
   void to_stream(std::ostream& os, bool print_items = false) const;
+
 private:
   static const uint8_t LG_MIN_MAP_SIZE = 3;
   static const uint8_t SERIAL_VERSION = 1;
