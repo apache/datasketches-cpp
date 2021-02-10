d35783ef8e217cd053f3c9d31bb80540e00802fb
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2021-02-10 14:09:40 -0800

    try this format

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='datasketches',
-    version='2.2.0',
+    version='2.2.0.dev0',
     author='Apache Datasketches Developers',
     author_email='dev@datasketches.apache.org',
     description='A wrapper for the C++ Apache Datasketches library',
