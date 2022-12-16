61df120156f7f87960627395f2726e029b7e2cab
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2022-12-15 19:55:51 -0800

    added link to the prototype in Python

--- density/include/density_sketch.hpp
+++ density/include/density_sketch.hpp
@@ -32,6 +32,9 @@
  * Based on the following paper:
  * Zohar Karnin, Edo Liberty "Discrepancy, Coresets, and Sketches in Machine Learning"
  * https://proceedings.mlr.press/v99/karnin19a/karnin19a.pdf
+ *
+ * Inspired by the following implementation:
+ * https://github.com/edoliberty/streaming-quantiles/blob/f688c8161a25582457b0a09deb4630a81406293b/gde.py
  */
 
 namespace datasketches {
