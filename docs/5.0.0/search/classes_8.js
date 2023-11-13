1fb163d6098be7132a5d561ea95a7e518b309fb0
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2023-11-13 23:34:48 +0000

    deploy: b39e41be2b743acb10f0b8b0bb491524fb17c77a

---
+++ docs/5.0.0/search/classes_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['kll_5fsketch_236',['kll_sketch',['../classdatasketches_1_1kll__sketch.html',1,'datasketches']]],
+  ['kolmogorov_5fsmirnov_237',['kolmogorov_smirnov',['../classdatasketches_1_1kolmogorov__smirnov.html',1,'datasketches']]]
+];
---
+++ docs/5.0.0/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['apache_20datasketches_20core_20c_2b_2b_20library_20component_442',['Apache DataSketches Core C++ Library Component',['../index.html',1,'']]]
+];
