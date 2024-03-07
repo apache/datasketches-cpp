9a6e2e3520b71d6127e50a0b7be937d1f116ee9a
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2024-03-07 21:09:41 +0000

    deploy: d2f7031529c2bb1444063e1dc908cc63cc9c35b8

---
+++ docs/5.0.2/search/classes_9.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['quantiles_5fsketch_238',['quantiles_sketch',['../classdatasketches_1_1quantiles__sketch.html',1,'datasketches']]],
+  ['quantiles_5fsorted_5fview_239',['quantiles_sorted_view',['../classdatasketches_1_1quantiles__sorted__view.html',1,'datasketches']]],
+  ['quantiles_5fsorted_5fview_3c_20t_2c_20c_2c_20a_20_3e_240',['quantiles_sorted_view&lt; T, C, A &gt;',['../classdatasketches_1_1quantiles__sorted__view.html',1,'datasketches']]],
+  ['quantiles_5fsorted_5fview_3c_20t_2c_20std_3a_3aless_3c_20t_20_3e_2c_20std_3a_3aallocator_3c_20t_20_3e_20_3e_241',['quantiles_sorted_view&lt; T, std::less&lt; T &gt;, std::allocator&lt; T &gt; &gt;',['../classdatasketches_1_1quantiles__sorted__view.html',1,'datasketches']]]
+];
---
+++ docs/5.0.2/search/functions_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['has_5fresult_363',['has_result',['../classdatasketches_1_1theta__intersection__alloc.html#a4ef54f76786bbe6e50c2597943874de3',1,'datasketches::theta_intersection_alloc::has_result()'],['../classdatasketches_1_1tuple__intersection.html#a4ef54f76786bbe6e50c2597943874de3',1,'datasketches::tuple_intersection::has_result()']]],
+  ['hll_5funion_5falloc_364',['hll_union_alloc',['../classdatasketches_1_1hll__union__alloc.html#abdb81912e4fcd2547e27c0165affbb99',1,'datasketches::hll_union_alloc']]]
+];
