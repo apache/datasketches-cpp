c58bb1572631cda3da565e4718076c8f39434b3e
geonove <andrea.novellini@outlook.it>
2025-08-17 15:52:24 +0200

    Clean tests

---
+++ ddsketch/test/StoreTest.cpp
@@ -0,0 +1,262 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <catch2/catch.hpp>
+
+#include "collapsing_highest_dense_store.hpp"
+#include "sparse_store.hpp"
+#include "store_factory.hpp"
+#include <iostream>
+
+#include "collapsing_lowest_dense_store.hpp"
+#include "unbounded_size_dense_store.hpp"
+
+namespace datasketches {
+
+using A = std::allocator<uint64_t>;
+
+template<class T>
+class bins_transformer_factory {
+public:
+  static std::unique_ptr<T> new_bins_transformer() {
+    return std::make_unique<T>();
+  }
+};
+
+// This
+template<const int max_num_bins>
+class collapsing_lowest_bins {
+public:
+  static std::vector<Bin> collapse(std::vector<Bin>& bins) {
+    int max_index = INT_MIN;
+    for (const Bin& bin : bins) {
+      max_index = std::max(max_index, bin.getIndex());
+    }
+    if (max_index < INT_MIN + max_num_bins) {
+      return bins;
+    }
+    int min_collapsed_index = max_index - max_num_bins + 1;
+    std::vector<Bin> collapsed_bins;
+    collapsed_bins.reserve(bins.size());
+    for (const Bin& bin : bins) {
+      collapsed_bins.emplace_back(std::max(bin.getIndex(), min_collapsed_index), bin.getCount());
+    }
+    return collapsed_bins;
+  }
+};
+
+template<const int max_num_bins>
+class collapsing_highest_bins {
+public:
+  static std::vector<Bin> collapse(std::vector<Bin>& bins) {
+    int min_index = INT_MAX;
+    for (const Bin& bin : bins) {
+      min_index = std::max(min_index, bin.getIndex());
+    }
+    if (min_index > INT_MAX - max_num_bins) {
+      return bins;
+    }
+    int max_collapsed_index = min_index + max_num_bins - 1;
+    std::vector<Bin> collapsed_bins;
+    collapsed_bins.reserve(bins.size());
+    for (const Bin& bin : bins) {
+      collapsed_bins.emplace_back(std::min(bin.getIndex(), max_collapsed_index), bin.getCount());
+    }
+    return collapsed_bins;
+  }
+};
+
+class noops_collapsing_bins {
+public:
+  static std::vector<Bin> collapse(std::vector<Bin>& bins) {
+    return bins;
+  }
+
+};
+
+std::vector<Bin> normalize_bins(const std::vector<Bin>& bins) {
+  std::map<int, uint64_t> bins_by_index;
+  for (const Bin& bin : bins) {
+    if (bin.getCount() <= 0) {
+      continue;
+    }
+    bins_by_index[bin.getIndex()] += bin.getCount();
+  }
+
+  std::vector<Bin> normalized_bins;
+  normalized_bins.reserve(bins_by_index.size());
+  for (auto & it : bins_by_index) {
+    normalized_bins.emplace_back(it.first, it.second);
+  }
+
+  std::sort(normalized_bins.begin(), normalized_bins.end(), [](const Bin& lhs, const Bin& rhs) {
+    return lhs.getIndex() < rhs.getIndex();
+  });
+
+  return normalized_bins;
+}
+
+template<class StoreType>
+void assert_encode_bins(StoreType& store, const std::vector<Bin>& normalized_bins) {
+  uint64_t expected_total_count = 0;
+  for (const Bin& bin : normalized_bins) {
+    expected_total_count += bin.getCount();
+  }
+
+  if (expected_total_count == 0) {
+    REQUIRE(store->is_empty());
+    REQUIRE(store->get_total_count() == 0);
+    REQUIRE_THROWS_AS(store->get_min_index(), std::runtime_error);
+    REQUIRE_THROWS_AS(store->get_max_index(), std::runtime_error);
+  } else {
+    REQUIRE_FALSE(store->is_empty());
+    REQUIRE(store->get_total_count() == expected_total_count);
+
+    REQUIRE(store->get_min_index() == normalized_bins[0].getIndex());
+    REQUIRE(store->get_max_index() == normalized_bins[normalized_bins.size() - 1].getIndex());
+
+    std::vector<Bin> bins;
+    for (const Bin& bin : *store) {
+        bins.push_back(bin);
+      }
+
+    std::ranges::sort(bins, [](const Bin& lhs, const Bin& rhs) {
+      return lhs.getIndex() < rhs.getIndex();
+    });
+    REQUIRE(bins.size() == normalized_bins.size());
+    for (size_t i = 0; i < bins.size(); ++i) {
+      REQUIRE(bins[i] == normalized_bins[i]);
+    }
+  }
+}
+
+template<class StoreType>
+void test_copy(StoreType& store, const std::vector<Bin>& normalized_bins) {
+  auto store_copy = store->copy();
+  store->merge(*store_copy);
+  assert_encode_bins(store_copy, normalized_bins);
+  store->clear();
+  assert_encode_bins(store_copy, normalized_bins);
+  std::vector<Bin> empty_bins;
+  assert_encode_bins(store, empty_bins);
+
+  std::vector<Bin> permutated_bins = normalized_bins;
+  std::ranges::shuffle(permutated_bins, std::mt19937());
+  if (normalized_bins.size() > 1) {
+    REQUIRE(permutated_bins != normalized_bins);
+  }
+  for (const Bin& bin : permutated_bins) {
+    store->add(bin);
+  }
+
+  assert_encode_bins(store, normalized_bins);
+}
+
+template<class StoreType>
+void test_store(StoreType& store, const std::vector<Bin>& normalized_bins) {
+  assert_encode_bins(store, normalized_bins);
+  test_copy<StoreType>(store, normalized_bins);
+}
+
+TEMPLATE_TEST_CASE("store test empty", "[storetest]",
+  (store_factory<CollapsingLowestDenseStore<A>, 8>),
+  (store_factory<CollapsingLowestDenseStore<A>, 128>),
+  (store_factory<CollapsingLowestDenseStore<A>, 1024>),
+  (store_factory<CollapsingHighestDenseStore<A>, 8>),
+  (store_factory<CollapsingHighestDenseStore<A>, 128>),
+  (store_factory<CollapsingHighestDenseStore<A>, 1024>),
+  (store_factory<SparseStore<A>>),
+  (store_factory<UnboundedSizeDenseStore<A>>)
+  ) {
+  auto store = TestType::new_store();
+  std::vector<Bin> empty_bins{};
+  test_store(store, empty_bins);
+}
+
+TEMPLATE_TEST_CASE("store test add datasets", "[storetest]",
+  (std::pair<store_factory<CollapsingLowestDenseStore<A>, 8>, collapsing_lowest_bins<8>>),
+  (std::pair<store_factory<CollapsingLowestDenseStore<A>, 128>, collapsing_lowest_bins<128>>),
+  (std::pair<store_factory<CollapsingLowestDenseStore<A>, 1024>, collapsing_lowest_bins<1024>>),
+  (std::pair<store_factory<CollapsingHighestDenseStore<A>, 8>, collapsing_highest_bins<8>>),
+  (std::pair<store_factory<CollapsingHighestDenseStore<A>, 128>, collapsing_highest_bins<128>>),
+  (std::pair<store_factory<CollapsingHighestDenseStore<A>, 1024>, collapsing_highest_bins<1024>>),
+  (std::pair<store_factory<SparseStore<A>>, noops_collapsing_bins>),
+  (std::pair<store_factory<UnboundedSizeDenseStore<A>>, noops_collapsing_bins>)
+  ) {
+  std::vector<std::vector<int>> datasets{
+    {-1000},
+    {-1},
+    {0},
+    {1},
+    {1000},
+    {1000, 1000},
+    {1000, -1000},
+    {-1000, 1000},
+    {-1000, -1000},
+    {0, 0, 0, 0}
+  };
+  std::vector<uint64_t> counts{0, 1, 100};
+}
+
+TEMPLATE_TEST_CASE("store test add constant", "[storetest]",
+  (std::pair<store_factory<CollapsingLowestDenseStore<A>, 8>, collapsing_lowest_bins<8>>),
+  (std::pair<store_factory<CollapsingLowestDenseStore<A>, 128>, collapsing_lowest_bins<128>>),
+  (std::pair<store_factory<CollapsingLowestDenseStore<A>, 1024>, collapsing_lowest_bins<1024>>),
+  (std::pair<store_factory<CollapsingHighestDenseStore<A>, 8>, collapsing_highest_bins<8>>),
+  (std::pair<store_factory<CollapsingHighestDenseStore<A>, 128>, collapsing_highest_bins<128>>),
+  (std::pair<store_factory<CollapsingHighestDenseStore<A>, 1024>, collapsing_highest_bins<1024>>),
+  (std::pair<store_factory<SparseStore<A>>, noops_collapsing_bins>),
+  (std::pair<store_factory<UnboundedSizeDenseStore<A>>, noops_collapsing_bins>)
+  ) {
+  std::vector<int> indexes{-1000, -1, 0, 1, 1000};
+  std::vector<uint64_t> counts{0, 1, 2, 4, 5, 10, 20, 100, 1000, 10000};
+
+  for (int idx: indexes) {
+    for (uint64_t count: counts) {
+      auto storeAdd = TestType::first_type::new_store();
+      auto storeAddBin = TestType::first_type::new_store();
+      auto storeAddWithCount = TestType::first_type::new_store();
+      for (int i = 0; i < count; ++i) {
+        storeAdd->add(idx);
+        storeAddBin->add(Bin(idx, 1));
+        storeAddWithCount->add(idx, 1);
+      }
+      std::vector<Bin> bins{Bin(idx, count)};
+      std::vector<Bin> normalized_bins = normalize_bins(TestType::second_type::collapse(bins));
+      test_store<decltype(storeAdd)>(storeAdd, normalized_bins);
+      test_store<decltype(storeAddBin)>(storeAddBin, normalized_bins);
+      test_store<decltype(storeAddWithCount)>(storeAddWithCount, normalized_bins);
+    }
+  }
+}
+
+TEST_CASE("merge test", "[mergetest]") {
+  CollapsingHighestDenseStore<std::allocator<uint8_t>> s1(1024);
+  CollapsingHighestDenseStore<std::allocator<uint8_t>> s2(1024);
+  CollapsingLowestDenseStore<std::allocator<uint8_t>> s3(1024);
+
+  DenseStore<std::allocator<uint8_t>>& sr = s2;
+
+  s1.merge(s2);
+  s1.merge(s3);
+  s1.merge(sr);
+}
+
+}
+
