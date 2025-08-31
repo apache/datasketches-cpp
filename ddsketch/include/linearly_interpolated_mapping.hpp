81448465a99f71dcd99afb599d30eb152adc6745
geonove <andrea.novellini@outlook.it>
2025-08-31 12:47:13 +0200

    Implement linearly interpolated mapping

---
+++ ddsketch/include/linearly_interpolated_mapping.hpp
@@ -0,0 +1,39 @@
+//
+// Created by Andrea Novellini on 30/08/2025.
+//
+
+#ifndef LINEARLY_INTERPOLATED_MAPPING_HPP
+#define LINEARLY_INTERPOLATED_MAPPING_HPP
+
+#include "index_mapping.hpp"
+#include "log_like_index_mapping.hpp"
+#include <numbers>
+
+
+namespace datasketches {
+
+class LinearlyInterpolatedMapping : public LogLikeIndexMapping<LinearlyInterpolatedMapping>{
+public:
+  explicit LinearlyInterpolatedMapping(const double& relative_accuracy);
+
+  LinearlyInterpolatedMapping(const double& gamma, const double& index_offset);
+
+  static constexpr double correcting_factor();
+  static constexpr double base();
+
+  double log(const double& value) const override;
+  double log_inverse(const double& index) const override;
+
+  IndexMappingLayout layout() const override;
+
+private:
+  static constexpr double BASE = 2.0;
+  static constexpr double CORRECTING_FACTOR = 1 / std::numbers::log2e;
+
+  double index_offset_shift(const double& relative_accuracy) const;
+};
+}
+
+#include "linearly_interpolated_mapping_impl.hpp"
+
+#endif //LINEARLY_INTERPOLATED_MAPPING_HPP
---
+++ ddsketch/include/linearly_interpolated_mapping_impl.hpp
@@ -0,0 +1,44 @@
+//
+// Created by Andrea Novellini on 30/08/2025.
+//
+
+#ifndef LINEARLY_INTERPOLATED_MAPPING_IMPL_HPP
+#define LINEARLY_INTERPOLATED_MAPPING_IMPL_HPP
+#include "linearly_interpolated_mapping.hpp"
+#include <cmath>
+#include "fast_log2.hpp"
+
+namespace datasketches {
+
+inline LinearlyInterpolatedMapping::LinearlyInterpolatedMapping(const double& relative_accuracy):
+  LogLikeIndexMapping(compute_gamma(require_valid_relative_accuracy(relative_accuracy), CORRECTING_FACTOR), index_offset_shift(relative_accuracy)) {}
+
+inline LinearlyInterpolatedMapping::LinearlyInterpolatedMapping(const double& gamma, const double& index_offset):
+  LogLikeIndexMapping(gamma, index_offset) {}
+
+constexpr double LinearlyInterpolatedMapping::correcting_factor() {
+  return CORRECTING_FACTOR;
+}
+
+constexpr double LinearlyInterpolatedMapping::base() {
+  return BASE;
+}
+
+inline double LinearlyInterpolatedMapping::log(const double& value) const {
+  return precise_log2(value);
+}
+
+inline double LinearlyInterpolatedMapping::log_inverse(const double& index) const {
+  return precise_log2_inverse(index);
+}
+
+inline IndexMappingLayout LinearlyInterpolatedMapping::layout() const {
+  return IndexMappingLayout::LOG_LINEAR;
+}
+
+inline double LinearlyInterpolatedMapping::index_offset_shift(const double& relative_accuracy) const {
+  return 1 / (std::log1p(2 * relative_accuracy / (1 - relative_accuracy)));
+}
+}
+
+#endif //LINEARLY_INTERPOLATED_MAPPING_IMPL_HPP
